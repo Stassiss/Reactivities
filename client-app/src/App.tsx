f094e5872bbe258a57975069d6e59ca7040c425b
stassiss <stassiss2@inbox.lv>
2020-06-29 11:08:07 +0300

    Initialize Client-app

---
+++ client-app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+const App:React.FC = ()=> {
+  return (
+    <div className="App">
+     <ul>
+      <h1>adsfasdf</h1>
+     </ul>
+    </div>
+  );
+}
+
+export default App;
